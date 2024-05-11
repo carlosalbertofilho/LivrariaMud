9a06d4ef1960521ee4af3f0e198c393125a4f46a
carlosalbertofilho <solrac.cassf@hotmail.com>
2024-05-11 18:36:35 -0300

    #CrossCutting:
    The most important changes involve the enhancement of the application's architecture by introducing dependency injection configurations for both the database context and the service layer. These changes aim to improve the application's modularity, maintainability, and adherence to clean architecture principles by decoupling components and abstracting implementations.
    
    ### Summary of Changes:
    
    1. **Addition of Database Configuration to Dependency Injection Container**
       - A new class `AddDb` has been introduced to facilitate the configuration and addition of the `LivrariaMudContext` database context to the ASP.NET Core dependency injection container. This class includes a method `AddDbApp` that configures the `DbContext` with a SQL Server database provider using a connection string from the application's configuration. It ensures that the application can interact with the database through a decoupled and abstracted manner, promoting better maintainability and scalability.
    
    2. **Addition of Service Layer to Dependency Injection Container**
       - The `AddServices` class has been added to register application services and their interfaces with the dependency injection container. By introducing the `AddServiceApp` method, it abstracts the concrete implementations of the `IBookRepository` and `IBookService`, thereby facilitating a cleaner and more modular architecture. This change is crucial for achieving a decoupled design where the application's components interact through interfaces rather than concrete implementations.
    
    ### References to Code Changes:
    
    - **Database Configuration**: The `AddDb` class within the `LivrariaMud.CrossCutting.DependenciesApp` namespace, specifically the `AddDbApp` method, handles the addition and configuration of the database context.
    - **Service Layer Configuration**: The `AddServices` class, also in the `LivrariaMud.CrossCutting.DependenciesApp` namespace, through its `AddServiceApp` method, manages the registration of service layer components and their interfaces with the dependency injection system.

---
+++ LivrariaMud.CrossCutting/DependenciesApp/AddDb.cs
@@ -0,0 +1,21 @@
+﻿using LivrariaMud.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LivrariaMud.CrossCutting.DependenciesApp;
+
+public static class AddDb
+{
+    public static IServiceCollection AddDbApp
+        (this IServiceCollection service, IConfiguration configuration)
+    {
+        var connectionString  = configuration.GetConnectionString("DefaultConnection")
+            ?? throw new InvalidOperationException("Não consegue conectar no banco de dados!");
+
+        service.AddDbContext<LivrariaMudContext>( options =>
+            options.UseSqlServer( connectionString ) );
+
+        return service;
+    }
+}
---
+++ LivrariaMud.CrossCutting/DependenciesApp/AddServices.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using LivrariaMud.Application.Services;
+using LivrariaMud.Application.Abstrations;
+using LivrariaMud.Domain.Abstrations;
+using LivrariaMud.Infrastructure.Repositories;
+
+namespace LivrariaMud.CrossCutting.DependenciesApp;
+
+public static class AddServices
+{
+    public static IServiceCollection AddServiceApp
+        (this IServiceCollection service)
+    {
+        service.AddScoped<IBookRepository, BookRepository>();
+        service.AddScoped<IBookService, BookServices>();
+        return service;
+    }
+}
